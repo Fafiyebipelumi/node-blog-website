b68e0953d26506936e337f777afa4936359e2c7a
Oluwapelumi <fafyeepelumi04@gmail.com>
2022-05-23 16:50:35 -0700

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // Connect to database
 const dbURI = 'mongodb+srv://Fafyee:oluwapelumi@blog-website.bivok.mongodb.net/Node-tut?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => console.log('Connected to database'))
+    .then((result) => console.log('Successfully connected to database'))
     .catch((err) => console.error(err))
 // Register View Engine
 app.set('view engine', 'ejs');
@@ -102,4 +102,4 @@ app.get('/blogs/create', (req, res) => {
 // 404
 app.use((req, res) => {
     res.status(404).render('404', { title: '404 - Not Found' });
-})
+});
